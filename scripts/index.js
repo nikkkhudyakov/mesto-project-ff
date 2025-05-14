b78510869c494e1c988f85daa51efaec77a946d3
Nikita Khudyakov <nikkhudyakov@mail.ru>
2025-05-14 18:01:44 +0300

    first version of project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderInitialCards() {
+  if (!initialCards || initialCards.length === 0) {
+    console.error('Вы не добавили ни одной карточки!');
+    return;
+  }
+
+  initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    cardsContainer.append(cardElement);
+  });
+}
+
+renderInitialCards();
